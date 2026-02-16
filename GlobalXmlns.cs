ea87d6e142278745f38b2962200397f4097330c4
Dylan Kopitzke <dbkopitzke@gmail.com>
2026-02-16 01:24:12 -0800

    Push blank maui

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MOBWEB_TEST")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MOBWEB_TEST.Pages")]
